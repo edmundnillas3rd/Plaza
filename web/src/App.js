d2e34bcb268c3f9043441067993b41d7170d6d95
Edmund Nillas III <edmundnillas3rd@gmail.com>
2022-12-30 18:14:26 +0800

    Add a login form component

--- web/src/App.js
+++ web/src/App.js
@@ -2,8 +2,13 @@ import "../src/style/styles.scss";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import Layout from "./components/layouts/Main";
+import LoginCard from "./components/LoginCard";
 
 function App() {
+  if (true) {
+    return <LoginCard />;
+  }
+
   return (
     <Router>
       <Layout />
