f744a9ba409665d15c5e48dd335110b17f60229b
Edmund Nillas III <edmundnillas3rd@gmail.com>
2022-12-30 18:14:26 +0800

    Add a login form component

--- web/src/components/ItemDescription.jsx
+++ web/src/components/ItemDescription.jsx
@@ -1,25 +1,47 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+const ReviewDisplay = ({ data }) => {
+  return (
+    <div className="review-container">
+      {data.reviews.map((review) => {
+        return (
+          <div className="review-card">
+            <div className="header">
+              <p>{review.user.name}</p>
+              <p>{review.rating}</p>
+            </div>
+            <p>{review.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default function ItemDescription() {
-  const [item, setItem] = useState(null);
+  const [data, setData] = useState(null);
 
   const { id } = useParams();
 
   useEffect(() => {
     fetch(`${id}`)
       .then((res) => res.json())
-      .then((data) => setItem(data.result.item));
-  }, [id]);
+      .then((data) => setData(data.result));
+  }, []);
 
   return (
     <div className="item-description-container">
-      {item ? (
-        <div>
-          <p>Name: {item.name}</p>
-          <p>Owner: {item.owner.name}</p>
-          <p>Price: {item.price}</p>
-        </div>
+      {data ? (
+        <>
+          <div className="item-main-description">
+            <p>Name: {data.item.name}</p>
+            <p>Owner: {data.item.user.name}</p>
+            <p>Price: {data.item.price}</p>
+            <p>Stock: {data.item.stock}</p>
+          </div>
+          <ReviewDisplay data={data} />
+        </>
       ) : (
         <p>Loading...</p>
       )}
