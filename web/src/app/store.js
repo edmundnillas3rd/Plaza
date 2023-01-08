d4c0482a5db4b20b5b7f7071945751ac748e7dc0
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-01-08 09:00:38 +0800

    Remove redux tutorial files and style the profile container

--- web/src/app/store.js
+++ web/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import userReducer from "../features/profile/userSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer
   }
 });
