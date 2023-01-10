6cc3f4704f39018fae10cc614daa36426834967e
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-01-10 09:32:18 +0800

    Refactor to include user id for login session

--- server/utils/passportConfig.js
+++ server/utils/passportConfig.js
@@ -37,7 +37,7 @@ module.exports = function (passport) {
   });
 
   passport.deserializeUser(function (id, done) {
-    User.findById({ _id: id }, function (err, user) {
+    User.findById(id, function (err, user) {
       done(err, user);
     });
   });
