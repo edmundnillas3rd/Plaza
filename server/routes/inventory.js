eac5b9dc8070f3a75724c0eed9d9ba524204333d
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-01-10 12:26:13 +0800

    Add feature for selling new items from the user

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const item_catalog = require("../controllers/itemController");
 
 router.get("/", item_catalog.index);
-
+router.post("/items", item_catalog.new_item);
 router.get("/items/:id", item_catalog.item_detail);
 
 module.exports = router;
