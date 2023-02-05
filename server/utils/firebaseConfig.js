59208758d86b9aa61b0d7dc4acde4a30c67bc0d1
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-02-05 08:06:00 +0800

    Remove old firebase credentials

--- server/utils/firebaseConfig.js
+++ server/utils/firebaseConfig.js
@@ -3,25 +3,11 @@ const { initializeApp, applicationDefault } = require("firebase-admin/app");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 
-// let admin = require("firebase-admin");
-// let serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
 // For Firebase Admin SDK only
 const firebaseConfig = {
   storageBucket: "plaza-54500.appspot.com",
   credential: applicationDefault()
 };
-
-// For Firebase SDK only
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCh0PX8aUmrYhsNDiPy1WtgSwLx762D7iQ",
-//   authDomain: "plaza-54500.firebaseapp.com",
-//   projectId: "plaza-54500",
-//   storageBucket: "plaza-54500.appspot.com",
-//   messagingSenderId: "636820117840",
-//   appId: "1:636820117840:web:bc906861147e966cf27455",
-//   measurementId: "G-J3ZY8SWJBD",
-// };
 
 const app = initializeApp(firebaseConfig);
 
