6ab2d05a5b1c6f7940c59a6d6d038eac68c5a65f
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-02-21 08:58:17 +0800

    Minor restyling of item cards

--- web/src/components/MainPage/ItemCard.jsx
+++ web/src/components/MainPage/ItemCard.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const ItemCard = ({ name, description, price, url, image }) => {
   const currency = "P";
   return (
-    <div className="item-card">
-      <Link to={url}>
+    <Link to={url}>
+      <div className="item-card">
         <div className="image-container">
           {image !== undefined ? (
             <img src={image} alt="item" />
@@ -18,8 +18,8 @@ const ItemCard = ({ name, description, price, url, image }) => {
           <p>{description}</p>
           <h3>{`${currency} ${price}`}</h3>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
