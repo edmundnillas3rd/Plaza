2f1237995a79e53e420c77fee490501647a44cfc
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-03-17 15:10:30 +0800

    Revise styling of item cards in the index page for reponsiveness

--- web/src/components/MainPage/StarRatings.jsx
+++ web/src/components/MainPage/StarRatings.jsx
@@ -5,5 +5,14 @@ export const StarRatings = ({ rating }) => {
     ratings.push(<div className="star-ratings-container" key={i}></div>);
   }
 
+  if (rating < 5 && 5 - rating > 0) {
+    let remainingStars = 5 - rating;
+    for (let i = 0; i < remainingStars; i++) {
+      ratings.push(
+        <div className="star-ratings-container rating-0" key={i}></div>
+      );
+    }
+  }
+
   return <div className="ratings-container">{ratings}</div>;
 };
