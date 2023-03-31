322d3f8376cfb488fc5a09b206d2edce1cb3b8ec
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-03-31 17:05:50 +0800

    Add redirection to login/signup when trying to sell a new product

--- web/src/components/MainPage/SellItem.jsx
+++ web/src/components/MainPage/SellItem.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function SellItem() {
+const Form = () => {
   const navigate = useNavigate();
 
   const userID = useSelector((state) => state.user.id);
@@ -80,108 +80,130 @@ export default function SellItem() {
   };
 
   return (
-    <div className="product-container">
-      <form
-        className="product-form-container"
-        action=""
-        method="POST"
-        onSubmit={addItem}
-        encType="multipart/form-data"
-      >
-        <p>Enter the information about the new item</p>
-        <div className="form-input-container ">
-          <label htmlFor="name">Name: </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="form-input-container ">
-          <label htmlFor="stock">Stock: </label>
-          <input
-            type="text"
-            name="stock"
-            id="stock"
-            onChange={(e) => setStock(e.target.value)}
-          />
-        </div>
-        <div className="form-input-container ">
-          <label htmlFor="price">Price: </label>
-          <input
-            type="text"
-            name="price"
-            id="price"
-            onChange={(e) => setPrice(e.target.value)}
-          />
-        </div>
-        <div className="form-input-container ">
-          <label htmlFor="description">Description</label>
-          <textarea
-            name="description"
-            id="description"
-            cols="30"
-            rows="10"
-            onChange={(e) => setDescription(e.target.value)}
-          ></textarea>
-        </div>
+    <form
+      className="product-form-container"
+      action=""
+      method="POST"
+      onSubmit={addItem}
+      encType="multipart/form-data"
+    >
+      <p>Enter the information about the new item</p>
+      <div className="form-input-container ">
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="form-input-container ">
+        <label htmlFor="stock">Stock: </label>
+        <input
+          type="text"
+          name="stock"
+          id="stock"
+          onChange={(e) => setStock(e.target.value)}
+        />
+      </div>
+      <div className="form-input-container ">
+        <label htmlFor="price">Price: </label>
+        <input
+          type="text"
+          name="price"
+          id="price"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+      </div>
+      <div className="form-input-container ">
+        <label htmlFor="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="10"
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
 
-        <div className="form-input-container">
-          <label htmlFor="category">Category</label>
-          <select
-            name="category"
-            id="category"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setCategory(e.target.value);
-            }}
-          >
-            <option value="">-- Please choose a category</option>
-            {categories !== undefined &&
-              categories.map((category, index) => (
-                <option key={index} value={`${category.name}`}>
-                  {category.name}
-                </option>
-              ))}
-          </select>
-        </div>
-        <div className=" ">
-          <label htmlFor="images">Image</label>
-          <input
-            type="file"
-            multiple="multiple"
-            accept="image/*"
-            name="images"
-            id="images"
-            onChange={(e) => {
-              setImages([...e.target.files]);
-            }}
-          />
-        </div>
-        <div className="image-display-container">
-          {images.length !== 0
-            ? images.map((image, index) => (
-                <div className="product-image-container" key={index}>
-                  <img src={`${URL.createObjectURL(image)}`} alt="item" />
-                  <span
-                    onClick={() => {
-                      setImages([
-                        ...images.slice(0, index),
-                        ...images.slice(index + 1)
-                      ]);
-                    }}
-                  >
-                    &times;
-                  </span>
-                </div>
-              ))
-            : null}
-        </div>
-        <div className="button-container">
-          <button>Add Item</button>
-        </div>
-      </form>
+      <div className="form-input-container">
+        <label htmlFor="category">Category</label>
+        <select
+          name="category"
+          id="category"
+          onChange={(e) => {
+            console.log(e.target.value);
+            setCategory(e.target.value);
+          }}
+        >
+          <option value="">-- Please choose a category</option>
+          {categories !== undefined &&
+            categories.map((category, index) => (
+              <option key={index} value={`${category.name}`}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className=" ">
+        <label htmlFor="images">Image</label>
+        <input
+          type="file"
+          multiple="multiple"
+          accept="image/*"
+          name="images"
+          id="images"
+          onChange={(e) => {
+            setImages([...e.target.files]);
+          }}
+        />
+      </div>
+      <div className="image-display-container">
+        {images.length !== 0
+          ? images.map((image, index) => (
+              <div className="product-image-container" key={index}>
+                <img src={`${URL.createObjectURL(image)}`} alt="item" />
+                <span
+                  onClick={() => {
+                    setImages([
+                      ...images.slice(0, index),
+                      ...images.slice(index + 1)
+                    ]);
+                  }}
+                >
+                  &times;
+                </span>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button>Add Item</button>
+      </div>
+    </form>
+  );
+};
+
+export default function SellItem() {
+  const isLogin = useSelector((state) => state.user.isLogin);
+
+  return (
+    <div className="product-container container column">
+      {isLogin ? (
+        <Form />
+      ) : (
+        <>
+          <p>Login with your account or sign up</p>
+          <div className="redirect-container container">
+            <button className="login-button">
+              <Link to="/login">Login to your account</Link>
+            </button>
+            <button className="signup-button">
+              <Link to="/signup">Sign Up now</Link>
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
