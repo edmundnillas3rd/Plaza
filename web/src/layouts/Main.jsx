de410fc63cb763c1f649d7b5e5b1c6195db875c4
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-03-31 15:59:15 +0800

    Style search bar

--- web/src/layouts/Main.jsx
+++ web/src/layouts/Main.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   AiOutlineShoppingCart,
   AiOutlineCopyrightCircle,
@@ -14,24 +14,32 @@ import Profile from "../features/profile/Profile";
 export default function Main({ categories, children }) {
   const [dropdown, setDropdown] = useState(false);
   const isLogin = useSelector((state) => state.user.isLogin);
+
   const [item, setItem] = useState("");
+
+  const navigate = useNavigate();
 
   return (
     <>
       <header>
         <h3>Plaza</h3>
         <div className="search-bar-container">
-          <input
-            type="text"
-            onChange={(e) => {
-              setItem(e.target.value);
+          <form
+            action=""
+            onSubmit={() => {
+              navigate(`/inventory/items/search/${item}`);
             }}
-          />
-          <div className="search-icon-container">
-            <Link to={`/inventory/items/search/${item}`}>
-              <AiOutlineSearch color="black" />
-            </Link>
-          </div>
+          >
+            <input
+              type="text"
+              onChange={(e) => {
+                setItem(e.target.value);
+              }}
+            />
+            <button>
+              <AiOutlineSearch size={16} />
+            </button>
+          </form>
         </div>
         <nav className="links-container container">
           <div className="nav-bar">
