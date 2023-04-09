03d1950bf8afc77eac437056f30b3cc2f79333e5
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-09 17:50:14 +0800

    Configure MongoStore and disable strict query for MongoDB

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    await mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("MongoDB Established Connection");
   } catch (error) {
