c768f9d636ecb55a29bb815d976c6d2e6bf1c692
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-28 08:43:04 +0800

    Remove depency of async package in item controller/route modules

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -5,10 +5,13 @@ const upload = multer({ dest: "images/" });
 
 const item_catalog = require("../controllers/itemController");
 
+// GET
 router.get("/", item_catalog.index);
 router.get("/items", item_catalog.item_categories);
 router.get("/items/search/:item_name", item_catalog.item_search);
 router.get("/items/:id", item_catalog.item_detail);
+
+// POST
 router.post("/items", upload.array("images"), item_catalog.new_item);
 router.post("/items/:id/reviews", item_catalog.item_review);
 
