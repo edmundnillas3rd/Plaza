6ec8a27ad2794e297e15f0f3c8f3ec128e8cbf23
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-04-28 09:06:19 +0800

    Refactor order and user controller and route modules

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 const user_controller = require("../controllers/userController");
 
+// GET
 router.get("/", user_controller.index);
+router.get("/logout", user_controller.log_out);
+
+// POST
 router.post("/signup", user_controller.sign_up);
 router.post("/login", user_controller.log_in);
-router.get("/logout", user_controller.log_out);
 
 module.exports = router;
