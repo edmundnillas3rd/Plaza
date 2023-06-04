61a27bd11ff5f28b4c5148ad0fe8b5e1f8fc79ac
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-06-04 15:18:04 +0800

    Remove all files (branch: dev-overhaul)

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./style/styles.scss";
-import Home from "./components/MainPage/Home";
 
 function App() {
   return (
     <Router>
-        <Home />
+        <div>Hello World!</div>
     </Router>
   );
 }
