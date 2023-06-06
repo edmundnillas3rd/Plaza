56cda41942a229b77dc68f016ca03e25d29b2346
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-06-06 18:38:43 +0800

    Add a landing page img banner (branch: dev-overhaul)

--- web/src/router/router.js
+++ web/src/router/router.js
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Layout from "../layouts/Main";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [{ index: true, element: <Home /> }]
   }
 ]);
 
