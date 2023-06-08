f947f5a662d7d3307c3494fdfaadb53f7f086989
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-06-08 19:26:38 +0800

    Restyle landing page img banner (branch: dev-overhaul)

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-      <div className="img-banner">
+    <div className="img-banner">
       <img
         src="https://images.unsplash.com/photo-1513672494107-cd9d848a383e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80"
         alt="boxes"
