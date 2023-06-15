36d1de3408071a5c33ce53843324aa56ad72df0e
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-06-15 20:27:52 +0800

    Add tentative component for handling user profile (branch: dev-overhaul)

---
+++ web/src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import userReducer from "../app/userSlice";
+
+export default configureStore({
+  reducer: {
+    userReducer
+  }
+});
---
+++ web/src/features/Profile.jsx
@@ -0,0 +1,10 @@
+import { MdOutlineAccountCircle } from "react-icons/md";
+
+export default function Profile() {
+  return (
+    <button className="container reset-justify align padded-md">
+      <MdOutlineAccountCircle />
+      <p>Account</p>
+    </button>
+  );
+}
