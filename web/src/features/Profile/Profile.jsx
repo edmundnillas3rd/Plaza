2c75e23cf89a3d62e06f416d13ff3f9161f16a72
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-06-16 14:14:36 +0800

    Style the profile component for dropdown and logging out user (branch: dev-overhaul)

--- web/src/features/Profile/Profile.jsx
+++ web/src/features/Profile/Profile.jsx
@@ -1,22 +1,54 @@
 import { MdOutlineAccountCircle } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { resetUser } from "../Profile/userSlice";
+import { useState } from "react";
 
 export default function Profile() {
   const user = useSelector((state) => state.user.username);
+  const token = useSelector((state) => state.user.token);
+
+  const [show, setShow] = useState(false);
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const isUserLogin = (e) => {
-    if (user.length === 0) navigate("/auth");
+  const isUserLogin = () => {
+    if (!!!user) {
+      navigate("/auth");
+      return;
+    }
+    setShow(!show);
+  };
+
+  const onLogoutHandler = () => {
+    fetch(`${process.env.REACT_APP_BASE_URL}/logout`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    dispatch(resetUser());
+    setShow(false);
+    localStorage.clear();
   };
 
   return (
-    <button
-      className="profile-container container reset-justify align gap-half padded-sm"
-      onClick={isUserLogin}
-    >
-      <MdOutlineAccountCircle size={24} />
-      <p>Account</p>
-    </button>
+    <div className="dropdown container reset-justfiy column">
+      <button
+        className="profile-container dropbtn container reset-justify align gap-half padded-sm"
+        onClick={isUserLogin}
+      >
+        <MdOutlineAccountCircle size={24} />
+        <p>{!!user ? user : "Account"}</p>
+      </button>
+
+      {show && (
+        <div className="dropdown-content container reset-justify column">
+          <button onClick={onLogoutHandler}>Logout</button>
+        </div>
+      )}
+    </div>
   );
 }
