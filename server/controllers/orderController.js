6b8aa7b6c88b7c828b06d43e33a6bc614d04276b
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-14 13:50:13 +0800

    Add condition for checking if user proceeded with the checkout session

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -42,50 +42,59 @@ exports.checkout_items = async (req, res, next) => {
     cancel_url: `${process.env.CLIENT_URL}`
   })
 
-  newOrders.orders.forEach(async (o) => {
-    const updatedItemStocks = await Item.findByIdAndUpdate(o.item, {
-      $inc: { stock: -o.quantity }
+  if (session.status === "complete")
+  {
+    newOrders.orders.forEach(async (o) => {
+      const updatedItemStocks = await Item.findByIdAndUpdate(o.item, {
+        $inc: { stock: -o.quantity }
+      });
+  
+      if (updatedItemStocks.stock <= 0) Item.findByIdAndRemove(o.item);
     });
+  
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "stmp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD
+      }
+    });
+  
+    const foundUser = await User.findById(user);
+  
+    const orderList = queriedOrders.orders.reduce(
+      (acc, currentValue, currentIndex, array) =>
+        acc + `<li>${array[currentIndex].name}</li>\n`,
+      ""
+    );
+  
+    let message = {
+      from: process.env.GMAIL_USER,
+      to: foundUser.email,
+      subject: "Plaza Order Receipt",
+      html: `
+            <h3>This is your order</h3>
+            <br>
+            <p>Orders:</p>
+            <ul>
+              ${orderList}
+            </ul>
+          `
+    };
+  
+    transporter.sendMail(message);
+  
+    res.status(200).json({
+      url: session.url
+    });
+  }
+  else {
+    res.status(200).json({
+      url: session.url
+    })
+  }
 
-    if (updatedItemStocks.stock <= 0) Item.findByIdAndRemove(o.item);
-  });
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "stmp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASSWORD
-    }
-  });
-
-  const foundUser = await User.findById(user);
-
-  const orderList = queriedOrders.orders.reduce(
-    (acc, currentValue, currentIndex, array) =>
-      acc + `<li>${array[currentIndex].name}</li>\n`,
-    ""
-  );
-
-  let message = {
-    from: process.env.GMAIL_USER,
-    to: foundUser.email,
-    subject: "Plaza Order Receipt",
-    html: `
-          <h3>This is your order</h3>
-          <br>
-          <p>Orders:</p>
-          <ul>
-            ${orderList}
-          </ul>
-        `
-  };
-
-  transporter.sendMail(message);
-
-  res.status(200).json({
-    url: session.url
-  });
 };
