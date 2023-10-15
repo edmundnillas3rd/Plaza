ce496ec2e418050c3a0e3387631f43393ad0d340
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-15 16:49:43 +0800

    Add webhooks for checking the fulfilled order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -12,38 +12,23 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_API_KEY);
 exports.index = async (req, res, next) => {};
 
 exports.checkout_items = async (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    res.status(401).json({
-      message: "Not Authenticated"
-    });
-    return;
-  }
+  const { orders } = req.body;
 
-  const { user, orders } = req.body;
+  const itemIDs = orders.map(product => product.item);
 
-  const order = new Order({
-    buyer: user,
-    orders: orders
-  });
+  const items = await Item.find({ _id: { "$all": itemIDs }  })
 
-  const newOrders = await order.save();
-
-  const queriedOrders = await newOrders.populate("orders.item");
-
-  const stripeLineItems = queriedOrders.orders.map(order => ({
-    price: order.item.stripe_price_id,
-    quantity: order.quantity
-  }))
-
-  const totalAmount = queriedOrders.orders.reduce((accumulator, currentValue, currentIndex, orders) => {
-    return accumulator + (orders[currentIndex].item.price * orders[currentIndex].quantity);
-  }, 0);
-
-  const stripePaymentIntent = await stripe.paymentIntents.create({
-    amount: totalAmount,
-    currency: "usd",
-    payment_method_types: ["card"]
+  const stripeLineItems = orders.map(product => {
+    const foundItem = items.find(item => item.id === product.item);
+    return {
+      price: foundItem.stripe_price_id,
+      quantity: product.quantity
+    }
   })
+
+  // const totalAmount = queriedOrders.orders.reduce((accumulator, currentValue, currentIndex, orders) => {
+  //   return accumulator + (orders[currentIndex].item.price * orders[currentIndex].quantity);
+  // }, 0);
 
   const session = await stripe.checkout.sessions.create({
     line_items: stripeLineItems,
@@ -52,18 +37,62 @@ exports.checkout_items = async (req, res, next) => {
     cancel_url: `${process.env.CLIENT_URL}`
   })
 
-  const { paymentIntent } = await stripe.confirmCardPayment(stripePaymentIntent.id);
+  res.status(200).json({
+    url: session.url
+  })
+};
 
-  if (paymentIntent && paymentIntent.status === "succeeded")
-  {
+exports.checkout_webhook = async (req, res) => {
+  const payload = req.body;
+
+  const sig = req.headers['stripe-signature'];
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(payload, sig, process.env.STRIPE_WEBHOOK_SECRET);
+  } catch (err) {
+    console.error('Webhook Error:', err.message);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  if (event.type === "checkout.session.completed") {
+
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ['line_items'],
+      }
+    );
+
+    const lineItems = sessionWithLineItems.line_items;
+
+    let orders = await lineItems.data.map(async item => {
+      const foundItem = await Item.find({ stripe_product_id: item.price.product });
+      return {
+        item: foundItem[0].id,
+        quantity: item.quantity
+      }
+    });
+
+    orders = await Promise.all(orders);
+
+    const buyer = await User.find({ email: event.data.object.customer_details.email });
+
+    const order = new Order({
+      buyer: buyer[0].id,
+      orders: orders
+    });
+
+    const newOrders = await order.save();
     newOrders.orders.forEach(async (o) => {
       const updatedItemStocks = await Item.findByIdAndUpdate(o.item, {
         $inc: { stock: -o.quantity }
       });
-  
-      if (updatedItemStocks.stock <= 0) Item.findByIdAndRemove(o.item);
+
+      if (updatedItemStocks.stock <= 0) 
+        Item.findByIdAndRemove(o.item);
     });
-  
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
       host: "stmp.gmail.com",
@@ -74,18 +103,17 @@ exports.checkout_items = async (req, res, next) => {
         pass: process.env.GMAIL_PASSWORD
       }
     });
-  
-    const foundUser = await User.findById(user);
-  
+
+    const queriedOrders = await newOrders.populate("orders.item");
     const orderList = queriedOrders.orders.reduce(
       (acc, currentValue, currentIndex, array) =>
-        acc + `<li>${array[currentIndex].name}</li>\n`,
+        acc + `<li>${array[currentIndex].item.name}</li>\n`,
       ""
     );
-  
+
     let message = {
       from: process.env.GMAIL_USER,
-      to: foundUser.email,
+      to: buyer[0].email,
       subject: "Plaza Order Receipt",
       html: `
             <h3>This is your order</h3>
@@ -96,17 +124,9 @@ exports.checkout_items = async (req, res, next) => {
             </ul>
           `
     };
-  
+
     transporter.sendMail(message);
-  
-    res.status(200).json({
-      url: session.url
-    });
-  }
-  else {
-    res.status(200).json({
-      url: session.url
-    })
   }
 
-};
+  res.status(200).end();
+}
