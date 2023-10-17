77f58c189d9587598bfe8d403463e59f096c1fbf
Edmund Nillas III <edmundnillas3rd@gmail.com>
2023-10-17 14:07:57 +0800

    Refactor styling for img item cards

--- web/src/components/ItemCard.jsx
+++ web/src/components/ItemCard.jsx
@@ -4,7 +4,9 @@ export default function ItemCard({ id, name, image, price, rating }) {
   return (
     <a href={`/products/item/${id}`}>
       <div className="item-card container column">
-        <img src={image} alt={name} />
+        <div className="img-container">
+          <img src={image} alt={name} />
+        </div>
         <div className="product-description container column gap-half">
           <div className="product-name">{name}</div>
           <div className="product-pricetag brand-orange">{price}</div>
